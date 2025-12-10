ea009f3a672c1c84d7560853783cc5e8e1e1d8c0
Vanshika Rupera <145897650+vanu23@users.noreply.github.com>
2025-12-10 14:43:43 +0530

    Add files via upload

---
+++ week_1/personal_intro.py
@@ -0,0 +1,13 @@
+# Personal Introduction Program
+# Day 1–5 Project | By Vanshika
+
+# Asking user for details
+name = input("What is your name? ")
+age = input("How old are you? ")
+hobby = input("What is your favorite hobby? ")
+city = input("Which city do you live in? ")
+
+# Displaying welcome message
+print("\n🎉 Welcome", name + "! 🎉")
+print(f"You are {age} years old, live in {city}, and love {hobby}.")
+print("Glad to meet you! 😊")
